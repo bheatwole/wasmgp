e8f8739cf6dbe041e4d9ce5d1b45f8cbc3f43261
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-10-10 09:23:47 -0400

    Added WorldConfiguration

--- wasmgp/src/lib.rs
+++ wasmgp/src/lib.rs
@@ -1,10 +1,12 @@
 mod migration_algorithm;
 mod selection_curve;
 mod threading_model;
+mod world_configuration;
 
 pub use migration_algorithm::MigrationAlgorithm;
 pub use selection_curve::SelectionCurve;
 pub use threading_model::ThreadingModel;
+pub use world_configuration::WorldConfiguration;
 
 #[cfg(test)]
 mod tests {
---
+++ wasmgp/src/world_configuration.rs
@@ -0,0 +1,44 @@
+use crate::{MigrationAlgorithm, SelectionCurve, ThreadingModel};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct WorldConfiguration {
+    /// The number of individuals on each island. Before running a generation, the island will be filled with the
+    /// children of genetic selection if there was a previous generation, or new random individuals if there was no
+    /// previous generation.
+    pub individuals_per_island: usize,
+
+    /// The number of individuals whose code will be copied as-is to the next generation. This can help preserve highly
+    /// fit code. Set to zero to disable elitism. ref https://en.wikipedia.org/wiki/Genetic_algorithm#Elitism
+    pub elite_individuals_per_generation: usize,
+
+    /// After this many generations across all islands, some of the individual will migrate to new islands. Set to zero
+    /// to disable automatic migrations.
+    pub generations_between_migrations: usize,
+
+    /// The number of individuals that will migrate from one island to another.
+    pub number_of_individuals_migrating: usize,
+
+    /// When it is time for a migration, a new island will be selected for the individual according to the specified
+    /// algorithm.
+    pub migration_algorithm: MigrationAlgorithm,
+
+    /// If false, individuals selected for migration are removed from their home island. If true, the selected
+    /// individuals are cloned and the clone is moved. The default is true
+    pub clone_migrated_individuals: bool,
+
+    /// The SelectionCurve that will be used when choosing which individual will participate in migration. The default
+    /// is PreferenceForFit.
+    pub select_for_migration: SelectionCurve,
+
+    /// The SelectionCurve that will be used when choosing a fit parent for genetic operations. The default is
+    /// PreferenceForFit.
+    pub select_as_parent: SelectionCurve,
+
+    /// The SelectionCurve used when choosing an elite individual to preserve for the next generation. The default is
+    /// StrongPreferenceForFit.
+    pub select_as_elite: SelectionCurve,
+
+    /// Determine how the world runs with regards to multi-threading. Placeholder: currently multi-threading is not
+    /// implemented
+    pub threading_model: ThreadingModel,
+}
