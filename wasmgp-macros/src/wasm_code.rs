311bb5ed83c75226ec87922e361838e0cabd5f4d
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-11-21 07:15:13 -0500

    Staring point for main body

--- wasmgp-macros/src/wasm_code.rs
+++ wasmgp-macros/src/wasm_code.rs
@@ -15,8 +15,9 @@ pub fn handle_macro(slot_count: &SlotCount, inner_fn: &mut ItemFn) -> Result<Tok
     // The visibility of the function becomes the visibility of the struct and methods
     let visibility = inner_fn.vis.clone();
 
-    // Pull the name of the function. This name (converted to PascalCase is also the name of the struct)
+    // Pull the name of the function. This name converted to PascalCase is also the name of the struct
     let function_name = inner_fn.sig.ident.to_string();
+    let function_name_lit = Lit::Str(LitStr::new(function_name.as_str(), inner_fn.sig.ident.span()));
     let struct_name: Ident =
         syn::parse_str::<Ident>(&format!("{}", function_name.to_case(Case::Pascal)))?;
 
@@ -36,6 +37,43 @@ pub fn handle_macro(slot_count: &SlotCount, inner_fn: &mut ItemFn) -> Result<Tok
         #visibility struct #struct_name {
             store: std::cell::RefCell<wasmtime::Store<#state_ident>>,
             func: wasmtime::TypedFunc<#param_generic, #result_generic>,
+        }
+
+        impl #struct_name {
+            fn new() -> anyhow::Result<#struct_name> {
+                let name = #function_name_lit;
+                let fs = crate::FunctionSignature::new(name, vec![crate::ValueType::I32], vec![crate::ValueType::I64]);
+                let slots = crate::SlotCount {
+                    i32: 0,
+                    i64: 0,
+                    f32: 0,
+                    f64: 0,
+                };
+                let context = crate::CodeContext::new(&fs, slots, false)?;
+                let code = vec![Code::Add(0, 0, 1), Code::Return];
+                let mut builder = wasm_ast::ModuleBuilder::new();
+                context.build(&mut builder, &code[..])?;
+                let module = builder.build();
+                let mut buffer = Vec::new();
+                wasm_ast::emit_binary(&module, &mut buffer)?;
+                let engine = wasmtime::Engine::default();
+                let module = wasmtime::Module::new(&engine, &buffer[..])?;
+                let mut store = wasmtime::Store::new(&engine, ());
+                let instance = wasmtime::Instance::new(&mut store, &module, &vec![])?;
+                let func = instance.get_typed_func::<#param_generic, #result_generic, _>(&mut store, name)?;
+    
+                Ok(#struct_name {
+                    store: std::cell::RefCell::new(store),
+                    func,
+                })
+            }
+    
+            fn call(&self, value: u32) -> anyhow::Result<#result_generic> {
+                use std::ops::DerefMut;
+                let mut store = self.store.borrow_mut();
+                let results = self.func.call(store.deref_mut(), value)?;
+                Ok(results)
+            }
         }
     }
     .into())
