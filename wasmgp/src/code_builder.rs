24d4067042b7f484a17ecbdb5c854e3b27a72dc4
Benjamin Heatwole <bheatwole@users.noreply.github.com>
2022-12-02 07:12:04 -0500

    Created default implement of make_random_code

--- wasmgp/src/code_builder.rs
+++ wasmgp/src/code_builder.rs
@@ -1,6 +1,7 @@
 use crate::indentation::Indentation;
 use crate::{code_context::CodeContext, Code};
 use anyhow::Result;
+use rand::rngs::SmallRng;
 use wasm_ast::Instruction;
 
 pub trait CodeBuilder {
@@ -8,7 +9,9 @@ pub trait CodeBuilder {
     fn append_code(&self, context: &CodeContext, instruction_list: &mut Vec<Instruction>) -> Result<()>;
 
     /// Creates a new random piece of code based on the parameters of the implementor
-    //fn make_random_code(rng: &mut SmallRng, max_points: usize) -> Code;
+    fn make_random_code(rng: &mut SmallRng, max_points: usize) -> Code {
+        panic!("this CodeBuilder should not be created as random code")
+    }
 
     /// Implementor should print the code in such a way as to be able to copy-paste to rust code files.
     fn print_for_rust(&self, _f: &mut std::string::String, _indentation: &mut Indentation) -> std::fmt::Result;
